e0e07c518737a50e26db5cfcdaab836032c1d7ab
ryan.gosling <gorocsmartonxd@gmail.com>
2024-09-06 09:13:11 +0200

    Argumentumok

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Csharp_first;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Az első argumentum: "+ args[0]);
+
+        Console.Write("1. sor");
+        Console.WriteLine("2.sor");
+    }
+}
